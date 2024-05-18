df9627bae1b315d12f41a4961f89ec0be8b04a64
Amariah <amariah.abish@gmail.com>
2024-05-18 13:20:12 +0300

    full test

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    name = input()
+    print("Hello,", name)
 
 def greet_with_default(name="programmer"):
-    pass
+    name = input()
+    if name != 'programmer':
+        print("Hello", name)
+    else:
+        print("Hello programmer")
 
 def add(num1, num2):
-    pass
+    num1 = int(input())
+    num2 = int(input())
+    add = num1 + num2
+    print(add)
 
 def halve(number):
-    pass
+    number = int(input())
+    halve = number // 2
+    print(halve)
