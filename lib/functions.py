568336605d37f8eb2cefef8e0f7c8ea4b4bac50c
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2024-05-18 16:17:09 +0300

    full test

--- lib/functions.py
+++ lib/functions.py
@@ -1,41 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    return "Hello, programmer!"
+    print("Hello, programmer!")
 
-def greet(name):
-    return f"Hello, {name}"
+greet_programmer()
+
+
+def greet(name = "John"):
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    if name != "programmer":
-        return f"Hello, {name}"
-    else:
-        return "Hello programmer"
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
     return num1 + num2
 
+
 def halve(number):
-    return number // 2
-
-
-
-# Usage of greet function with user input
-name = input("Enter your name: ")
-print(greet(name))
-
-# Usage of greet_with_default function with user input
-name_with_default = input("Enter your name or press enter to use default: ")
-if name_with_default == "":
-    print(greet_with_default())
-else:
-    print(greet_with_default(name_with_default))
-
-# Usage of add function with user input
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-print(add(num1, num2))
-
-# Usage of halve function with user input
-number = int(input("Enter a number to halve: "))
-print(halve(number))
+    return number/2
